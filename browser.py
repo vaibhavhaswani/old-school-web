10e4b2c5e7d33c25303b1558af18e4b58a40548a
Vaibhav Haswani <vaibhavhaswani@gmail.com>
2020-10-09 21:00:16 +0530

    indevelopment

---
+++ browser.py
@@ -0,0 +1,15 @@
+import socket
+
+sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)   #TCP/IP socket
+url=input("enter url:")
+suburls=url.split('/')
+sock.connect((suburls[0],80))  #server / port
+
+cmd=f'GET http://{url} HTTP/1.0\r\n\r\n'.encode()
+sock.send(cmd)
+
+while True:
+	data=sock.recv(512) 
+	if len(data)<1:
+		break
+	print(data.decode(),end='')
