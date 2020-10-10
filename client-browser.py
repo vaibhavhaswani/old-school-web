c200f7aaeb840ce92b501454a79affa87983a8e9
Vaibhav Haswani <vaibhavhaswani@gmail.com>
2020-10-10 15:46:55 +0530

    server added

--- client-browser.py
+++ client-browser.py
@@ -2,8 +2,9 @@ import socket
 
 sock=socket.socket(socket.AF_INET,socket.SOCK_STREAM)   #TCP/IP socket
 url=input("enter url:")
+port=int(input("Port to forward request:"))
 suburls=url.split('/')
-sock.connect((suburls[0],80))  #server / port
+sock.connect((suburls[0],port))  #server / port
 
 cmd=f'GET http://{url} HTTP/1.0\r\n\r\n'.encode()
 sock.send(cmd)
